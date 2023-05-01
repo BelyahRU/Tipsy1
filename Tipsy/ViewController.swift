5c610cb9e9ec799573ff7dd56a3a81e7206b9891
BelyahRU <oBelyash@yandex.ru>
2023-05-02 02:26:41 +0300

    add textField and Label

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -6,29 +6,75 @@ class ViewController: UIViewController {
     var secondView = UIView()
     var infoStackView = UIStackView()
     var enterBillTotalLabel = UILabel()
+    var egTextField = UITextField()
+    var calculateStackView = UIStackView()
+    var selectTipLabel = UILabel()
+    var buttonsPersentStackView = UIStackView()
+    var zeroPerButton = UIButton()
+    var tenPerButton = UIButton()
+    var twentyPerButton = UIButton()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         setupInfoStackView()
         setupSecondView()
+        setupEnterBillTotalLabel()
+        setupEgTextField()
+        print("safsa")
         
     }
     
     private func setupInfoStackView() {
+        
         view.addSubview(infoStackView)
-        infoStackView.backgroundColor = .gray
+        infoStackView.backgroundColor = .white
+        infoStackView.axis = .vertical
+        infoStackView.spacing = 26
+        infoStackView.alignment = .center
+        
+        
         infoStackView.snp.makeConstraints { make in
             make.left.equalToSuperview()
             make.right.equalToSuperview()
             make.top.equalToSuperview().offset(69)
             make.bottom.equalToSuperview().offset(-679)
         }
+        
+        infoStackView.addArrangedSubview(enterBillTotalLabel)
+        infoStackView.addArrangedSubview(egTextField)
     }
+    
+    private func setupEnterBillTotalLabel() {
+        enterBillTotalLabel.font = UIFont(name: enterBillTotalLabel.font.fontName, size: 25)
+        enterBillTotalLabel.text = "Enter bill total"
+        enterBillTotalLabel.textColor = #colorLiteral(red: 0.753274262, green: 0.7732002139, blue: 0.7771559358, alpha: 1)
+        enterBillTotalLabel.snp.makeConstraints { make in
+            make.width.equalTo(293)
+            make.height.equalTo(30)
+        }
+    }
+    
+    private func setupEgTextField() {
+        egTextField.text = "e.g. 123.56"
+        egTextField.font = UIFont(name: egTextField.font!.fontName, size: 40)
+        egTextField.textAlignment = .center
+        egTextField.textColor = #colorLiteral(red: 0.753274262, green: 0.7732002139, blue: 0.7771559358, alpha: 1)
+        egTextField.snp.makeConstraints { make in
+            make.left.right.equalToSuperview()
+        }
+    }
+    
+    
+    
+    
     
     private func setupSecondView() {
         view.addSubview(secondView)
-        secondView.backgroundColor = .red
+        secondView.backgroundColor = #colorLiteral(red: 0.8453745246, green: 0.976534903, blue: 0.9226958752, alpha: 1)
         secondView.snp.makeConstraints { make in
+            
             make.left.equalToSuperview()
             make.right.equalToSuperview()
             make.bottom.equalToSuperview()
