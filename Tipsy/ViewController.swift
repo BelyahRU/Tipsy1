de6c04b4ee525702218c6d7efafda5b8bf6d7eed
BelyahRU <oBelyash@yandex.ru>
2023-05-01 08:46:50 +0300

    make info StackView and secondView

--- Tipsy/ViewController.swift
+++ Tipsy/ViewController.swift
@@ -1,19 +1,38 @@
-//
-//  ViewController.swift
-//  Tipsy
-//
-//  Created by Александр Андреев on 01.05.2023.
-//
-
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var secondView = UIView()
+    var infoStackView = UIStackView()
+    var enterBillTotalLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupInfoStackView()
+        setupSecondView()
+        
     }
-
-
+    
+    private func setupInfoStackView() {
+        view.addSubview(infoStackView)
+        infoStackView.backgroundColor = .gray
+        infoStackView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalToSuperview().offset(69)
+            make.bottom.equalToSuperview().offset(-679)
+        }
+    }
+    
+    private func setupSecondView() {
+        view.addSubview(secondView)
+        secondView.backgroundColor = .red
+        secondView.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.top.equalTo(infoStackView.snp.bottom).offset(40)
+        }
+    }
 }
-
